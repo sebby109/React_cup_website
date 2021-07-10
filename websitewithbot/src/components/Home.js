7427e6a9387f9ea97b620f9023d3d2f0dd2bff9c
sebby109 <sebastianga14@hotmail.com>
2021-07-09 23:00:26 -0700

    Made a home comonent and tested it on app.js

---
+++ websitewithbot/src/components/Home.js
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container';
+
+function Home () {
+    return (
+        <Container>
+            Hello
+        </Container>
+    );
+}
+
+export default Home;
